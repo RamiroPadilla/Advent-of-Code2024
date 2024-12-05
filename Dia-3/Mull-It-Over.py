fe5af08f77ec02a477c30232ee6abb486c5f76f3
RamiroPadilla <ramiro_mdq@live.com>
2024-12-05 19:33:22 -0300

    dia 3

--- Dia-3/Mull-It-Over.py
+++ Dia-3/Mull-It-Over.py
@@ -13,3 +13,22 @@ with open('input.txt', 'r', encoding="utf-8") as f:
 
 print(res)
 # %%
+# Parte 2
+
+res_2 = 0
+
+with open('input.txt', 'r', encoding="utf-8") as f:   
+    do = True 
+    for line in f:
+        instruction = re.findall(r"mul\(\d+,\d+\)|do\(\)|don't\(\)", line)
+
+        for inst in instruction:
+            if(re.search(r"don't\(\)",inst)):
+                do = False
+            elif(re.search(r"do\(\)",inst)):
+                do = True
+            elif(do):
+                mults = re.search(r"(\d+),(\d+)", inst)
+                res_2 += int(mults.group(1)) * int(mults.group(2))  
+
+print(res_2)       
